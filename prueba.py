fe2e959caac406258db238763b7e94003185d442
Sari Briones <99497309+Sarahi-Briones@users.noreply.github.com>
2022-12-20 08:00:33 -0600

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -5,11 +5,15 @@ import pickle
 import numpy as np
 import statistics
 import sklearn.metrics as met
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import f1_score
 from sklearn.metrics import classification_report
 from sklearn.metrics import ConfusionMatrixDisplay
 from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
 
+#------------------------------------Clases-----------------------------------------
 class validation_set:
 	def __init__(self, X_train, y_train, X_test, y_test):
 		self.X_train = X_train
@@ -24,64 +28,39 @@ class data_set:
 	def __init__(self, validation_set, test_set):
 		self.validation_set = validation_set
 		self.test_set = test_set
-  
-# def Entrenamiento(X_train, y_train):
-#     i=0
-#     Class1=[]
-#     Class2=[]
-#     Class3=[]
-#     Class4=[]
-#     Class5=[]
-#     Diferencias_Totales=[]
-#     # for opinion in val_set.X_train:
-#     for opinion in X_train:
-#         PalabraDesconocida=0
-#         titulo_Positivo=0
-#         titulo_Negativo=0
-#         opinion_positivo=0
-#         opinion_negativo=0
-#         Diferencia_total_positivo=0
-#         Diferencia_total_negativo=0
-#         #Para los Titulos
-#         #print(opinion[0])
-#         for word in opinion[0]:
-#             try:
-#                 Sentimiento=dic.get(word)[0]
-#                 # if(Sentimiento=="Alegría" or Sentimiento =="Sorpresa"): # Alegría y Sorpresa
-#                 titulo_Positivo+=dic.get(word)[0]
-#                 # else:                                                   # Enojo Miedo Repulsión Tristeza
-#                 titulo_Negativo+=dic.get(word)[1]
-#             except:
-#                 PalabraDesconocida+=1
-#                 # print("No exite esa palabra en el diccionario")
-#         #Para las Opiniones
-#         for word in opinion[1]:
-#             try:
-#                 Sentimiento=dic.get(word)[1]
-#                 # if(Sentimiento=="Alegría" or Sentimiento =="Sorpresa"): # Alegría y Sorpresa
-#                 opinion_positivo+=dic.get(word)[0]
 
-#                 # else:                                                   # Enojo Miedo Repulsión Tristeza
-#                 opinion_negativo+=dic.get(word)[1]
-#             except:
-#                 PalabraDesconocida=PalabraDesconocida
-#         Diferencia_total_positivo=titulo_Positivo+opinion_positivo
-#         Diferencia_total_negativo=titulo_Negativo+opinion_negativo
-#         DiferenciaTotal=opinion_positivo-opinion_negativo
-#         Diferencias_Totales.append(DiferenciaTotal)
-#     #     if y_train[i]==1:
-#     #         Class1.append(DiferenciaTotal)
-#     #     elif y_train[i]==2:
-#     #         Class2.append(DiferenciaTotal)
-#     #     elif y_train[i]==3:
-#     #         Class3.append(DiferenciaTotal)
-#     #     elif y_train[i]==4:
-#     #         Class4.append(DiferenciaTotal)
-#     #     elif y_train[i]==5:
-#     #         Class5.append(DiferenciaTotal)
-#     #     i+=1
-#     # return Class1, Class2, Class3, Class4, Class5
-#     return Diferencias_Totales
+#-----------------------------------Funciones---------------------------------------
+def LematizadorOpiniones(opiniones,nlp):
+    opiniones_lematizadas=[]
+    for opinion in opiniones:
+        if str(opinion)!=str("nan"):
+            doc=nlp(opinion)
+            opinion_tokenizada=[]
+            opinion_lematizada=[]
+            opinion_tokenizada=[token for token in doc]
+            opinion_lematizada=[token.lemma_ for token in opinion_tokenizada]
+            str_opinion_lematizada=""
+            str_opinion_lematizada=" ".join(opinion_lematizada)
+            opiniones_lematizadas.append(str_opinion_lematizada)
+        else:
+            opiniones_lematizadas.append(" ")
+    return opiniones_lematizadas
+
+def LematizadorTitulos(titles,nlp):
+    titulos_lematizados=[]
+    for titulo in titles:
+        if str(titulo)!=str("nan"):
+            doc=nlp(str(titulo))
+            titulo_tokenizado=[]
+            titulo_lematizado=[]
+            titulo_tokenizado=[token for token in doc]
+            titulo_lematizado=[token.lemma_ for token in titulo_tokenizado]
+            str_titulo_lematizado=""
+            str_titulo_lematizado=" ".join(titulo_lematizado)
+            titulos_lematizados.append(str_titulo_lematizado)
+        else: 
+            titulos_lematizados.append(" ")
+    return titulos_lematizados
 
 def Entrenamiento(X_train, y_train):
     i=0
@@ -122,12 +101,9 @@ def Entrenamiento(X_train, y_train):
                 opinion_negativo+=dic.get(word)[1]
             except:
                 PalabraDesconocida=PalabraDesconocida
-                # PalabraDesconocida+=1
+                PalabraDesconocida+=1
                 # print("No exite esa palabra en el diccionario")
-        # print("Pos:"+str(opinion_positivo))
-        # print("Neg:"+str(opinion_negativo))
-        # val_set.y_train
-        # Data.append([opinion_positivo,opinion_negativo,y_train])
+
         Data.append([opinion_positivo,opinion_negativo])
         
         i+=1
@@ -143,33 +119,99 @@ def Entrenamiento(X_train, y_train):
         # print("Palabras Desconocidas: "+str(PalabraDesconocida))
     return Data,DiferenciaTotal,X_Diferencia_opinion
 
-
-def Evaluador(Diferencia):
+def Evaluador(Diferencia, UmbralesFinales):
     i=0
     ValoracionOpinion=[]
     for df in Diferencia:
-        if df <= -0.9:
+        if df <= UmbralesFinales[0]:
             ValoracionOpinion.append(1)
-        elif  df <= -0.4:
+        elif  df <= UmbralesFinales[1]:
             ValoracionOpinion.append(2)
-        elif  df <= -0.2:
+        elif  df <= UmbralesFinales[2]:
             ValoracionOpinion.append(3)
-        elif  df <= -0.1:
+        elif  df <= UmbralesFinales[3]:
             ValoracionOpinion.append(4)
-        elif df>-0.1:
+        elif df > UmbralesFinales[4]:
             ValoracionOpinion.append(5)
         else:
             ValoracionOpinion.append(2)
         # print("Valoracion Predicha Opinion["+str(i)+"]: "+str(ValoracionOpinion[len(ValoracionOpinion) - 1])+", valor real: "+str(y_train[i]))
         i+=1
     return ValoracionOpinion
-  
+
+#----------------------------------Inicio----------------------------------
+
+#Esta parte solo es para tokenizar y lematizar el corpus y despues guardar los resultados en un pickle
+
+# df = pd.read_excel('Rest_Mex_2022_Sentiment_Analysis_Track_Train.xlsx')
+
+# titles = df['Title'].values
+# opinions = df['Opinion'].values
+# polarity=df['Polarity'].values
+# attraction=df['Attraction'].values
+
+# #Cargamos corpus de spacy para tokenizar y lematizar
+# nlp=spacy.load('es_core_news_sm')
+
+
+# #Tokenizamos y lemmatizamos opiniones
+# opiniones_lematizadas=LematizadorOpiniones(opinions,nlp)
+# titulos_lematizados=LematizadorTitulos(titles,nlp)
+
+# #Guardamos opiniones y titulos lematizados en pickle
+
+# matriz_lematizados=[]
+# headers=[("Title"),("Opinion"),("Polarity")]
+# matriz_lematizados.append(headers)
+# for titulo_lematizado,opinion_lematizada,polaridad in zip(titulos_lematizados,opiniones_lematizadas,polarity):
+#     matriz_lematizados.append([titulo_lematizado,opinion_lematizada,polaridad])
+
+
+# #Guardamos matriz_lematizados en pickle
+# with open("matriz_opiniones_titulos_lematizados.pickle", "wb") as f:
+#     pickle.dump(matriz_lematizados, f)
+
 with open("matriz_opiniones_titulos_lematizados.pickle", "rb") as f:
     obj = pickle.load(f)
 df=pd.DataFrame(obj)
 
+# Praparación del diccionario de términos
+
+# #Carga del archivo txt a un dataframe para poder almacenarlo y convertirlo en diccionario
+# df_dic = pd.read_csv('SEL_full.txt', sep=str("\t"), header=None, names=['Palabra', 'Nula[%]','Baja[%]', 'Media[%]', 'Alta[%]', 'PFA', 'Categoría'], skiprows=1, encoding='utf-8')
+# df_dic.drop(['Nula[%]', 'Baja[%]', 'Media[%]', 'Alta[%]'], axis=1, inplace=True)
+# df_dic['PFA'].apply(lambda x: float(x))
+# palabras_unicas=df_dic.Palabra.unique()
+
+# valores=[]
+# for palabra in palabras_unicas:
+#     positivo=0.0
+#     negativo=0.0
+#     subdf= pd.DataFrame(df_dic[df_dic.Palabra==palabra])
+#     for i in range(len(subdf)):
+#         if str(subdf.iloc[i].Categoría)=="Alegría" or str(subdf.iloc[i].Categoría)=="Sorpresa":
+#             positivo= positivo + subdf.iloc[i].PFA
+#         elif str(subdf.iloc[i].Categoría)=="Enojo" or str(subdf.iloc[i].Categoría)=="Miedo" or str(subdf.iloc[i].Categoría)=="Repulsión" or str(subdf.iloc[i].Categoría)=="Tristeza":
+#             negativo= negativo + subdf.iloc[i].PFA
+#     valores.append([str(palabra), positivo, negativo])
+
+# df_diccionary=pd.DataFrame(valores, columns=['Palabra', 'Positivo', 'Negativo'])
+
+# #Convertimos a un diccionario
+# diccionario = df_diccionary.set_index('Palabra').T.to_dict('list')
+
+# #Guardamos el diccionario en un archivo .pkl
+# with open("Dictionary.pickle", "wb") as tf:
+#     pickle.dump(diccionario,tf)
+
 with open("Dictionary.pickle", "rb") as f:
     dic = pickle.load(f)
+
+# Pruebas del diccionario
+# print(dic)
+# for i in dic:
+#     print(i)
+# print(dic.get("alma"))
 
 #Guardamos los datos de entrenamiento en X
 X=[]
@@ -188,6 +230,7 @@ for i in range(len(obj)):
 #Dividimos en train y test
 X_train, X_test, y_train, y_test= train_test_split(X, y, test_size=0.2,random_state=0,shuffle= True)
 
+#-------------------------------------Pliegues--------------------------------------#
 validation_sets = []
 kf = KFold(n_splits=5)
 c=0
@@ -203,62 +246,54 @@ for train_index, test_index in kf.split(X_train):
 my_test_set = test_set(X_test, y_test)	
 my_data_set = data_set(validation_sets, my_test_set) 
 
-Umbrales1=[]
-Umbrales2=[]
-Umbrales3=[]
-Umbrales4=[]
-Umbrales5=[]
-UmbralesFinales=[]
+#-------------------------------------Entrenamiento--------------------------------------#
+
+# Umbrales obtenidos luego de hacer muchos expereimentos (prueba y error)
+UmbralesFinales=[-0.9, -0.4, -0.2, -0.1, -0.1]
+
 pliegue=1
 for val_set in my_data_set.validation_set:
     print("Pliegue: ",pliegue)
-    Umbral1=[]
-    Umbral2=[]
-    Umbral3=[]
-    Umbral4=[]
-    Umbral5=[]
+    
     #Datos de entrenamiento
     Data,DiferenciaTotal,X_Diferencia_opinion= Entrenamiento(val_set.X_train,val_set.y_train)
-    #umb1=[min(Umbral1), max(Umbral1)]
-    #umb1=[statistics.mode(Umbral1)]
-    #Umbrales1.append(umb1)
-    #umb2=[min(Umbral2), max(Umbral2)]
-    #umb2=[statistics.mode(Umbral2)]
-    #Umbrales2.append(umb2)
-    #umb3=[min(Umbral3), max(Umbral3)]
-    #umb3=[statistics.mode(Umbral3)]
-    #Umbrales3.append(umb3)
-    #umb4=[min(Umbral4), max(Umbral4)]
-    #umb4=[statistics.mode(Umbral4)]
-    #Umbrales4.append(umb4)
-    #umb5=[min(Umbral5), max(Umbral5)]
-    #umb5=[statistics.mode(Umbral5)]
-    #Umbrales5.append(umb5)
-    # print(Umbral5)
-    # print("\n\n")
+    ValoracionOpinion=Evaluador(X_Diferencia_opinion, UmbralesFinales)
+    yreal=[]
+    for id in val_set.y_train:
+        yreal.append(id)
+        
+    # Métricas para medir la eficiencia de cada experimento en cada pliegue
+    
+    # print('Accuracy:',met.accuracy_score(yreal, ValoracionOpinion)) 
+    # print('Precision:',precision_score(yreal, ValoracionOpinion, average='micro'))
+    # print('Recall: ', recall_score(yreal, ValoracionOpinion, average='micro'))
+    # print('F-Measure:', f1_score(yreal, ValoracionOpinion, average='micro'))
+    # print("\n")
     
     Data2,DiferenciaTotal,X_Diferencia_opinion=Entrenamiento(val_set.X_test, val_set.y_test)
-    ValoracionOpinion=Evaluador(X_Diferencia_opinion)
-    yreal=[]
-    for id in val_set.y_test:
-        yreal.append(id)
-    print('Accuracy:',met.accuracy_score(yreal, ValoracionOpinion)) 
-    pliegue+=1
+    ValoracionOpinion=Evaluador(X_Diferencia_opinion, UmbralesFinales)
+    # yreal=[]
+    # for id in val_set.y_test:
+    #     yreal.append(id)
+    # print('Accuracy:',met.accuracy_score(yreal, ValoracionOpinion)) 
     
-# UmbralesFinales.append(Umbrales1)
-# UmbralesFinales.append(Umbrales2)
-# UmbralesFinales.append(Umbrales3)
-# UmbralesFinales.append(Umbrales4)
-# UmbralesFinales.append(Umbrales5)
-# print(UmbralesFinales)
+    pliegue+=1
+print("\n")
 
+#-----------------------------Predicción en el conjunto de prueba----------------------------#
 Data2,DiferenciaTotal,X_Diferencia_opinion=Entrenamiento(my_test_set.X_test, my_test_set.y_test)
-ValoracionOpinion=Evaluador(X_Diferencia_opinion)
+ValoracionOpinion=Evaluador(X_Diferencia_opinion, UmbralesFinales)
 
 yreal=[]
 for id in my_test_set.y_test:
         yreal.append(id)
+print("\n")
 print('Accuracy:',met.accuracy_score(yreal, ValoracionOpinion)) 
+# print('Accuracy:',met.accuracy_score(yreal, ValoracionOpinion)) 
+# print('Precision:',precision_score(yreal, ValoracionOpinion, average='micro'))
+# print('Recall: ', recall_score(yreal, ValoracionOpinion, average='micro'))
+# print('F-Measure:', f1_score(yreal, ValoracionOpinion, average='micro'))
+# print("\n")
 target_names=["1","2","3","4","5"]
 print(classification_report(yreal, ValoracionOpinion, target_names=target_names))
 print (confusion_matrix(yreal, ValoracionOpinion))
